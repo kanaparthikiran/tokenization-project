6600645ea5cc3da6117b7b253feead4400144c0f
Kiran Kanaparthi <kkanaparthi@hotwire.com>
2019-07-24 00:33:26 -0700

    Added Kafka and Redis dependencies

---
+++ startKafka_Redis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Script to build the code and Run.
+
+sh dependencies/kafka_2.11-2.2.1/bin/zookeeper-server-start.sh dependencies/kafka_2.11-2.2.1/config/zookeeper.properties&
+
+sh dependencies/kafka_2.11-2.2.1/bin/kafka-server-start.sh dependencies/kafka_2.11-2.2.1/config/server.properties&
+
+chmod  +x dependencies/redis-stable/src/redis-server && dependencies/redis-stable/src/redis-server
