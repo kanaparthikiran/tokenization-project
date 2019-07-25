33651b431852cf36fd35e0739f417870d3b91eb1
Kiran Kanaparthi <kkanaparthi@hotwire.com>
2019-07-25 14:21:58 -0700

    Added Compressed Kafka Folder

--- start_kafka_redis.sh
+++ start_kafka_redis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script to build the code and Run.
-
-sh dependencies/kafka_2.11-2.2.1/bin/zookeeper-server-start.sh dependencies/kafka_2.11-2.2.1/config/zookeeper.properties&
-sh dependencies/kafka_2.11-2.2.1/bin/kafka-server-start.sh dependencies/kafka_2.11-2.2.1/config/server.properties&
+tar -xvf dependencies/kafka_2.11-2.2.1.tgz
+sh kafka_2.11-2.2.1/bin/zookeeper-server-start.sh kafka_2.11-2.2.1/config/zookeeper.properties&
+sh kafka_2.11-2.2.1/bin/kafka-server-start.sh kafka_2.11-2.2.1/config/server.properties&
 tar -xvf dependencies/redis-stable.tar
 chmod  +x redis-stable/src/redis-server && redis-stable/src/redis-server&
