21a5ad404e5de816bb592c6ae475abcbde0ac923
Kiran Kanaparthi <kkanaparthi@hotwire.com>
2019-07-25 11:31:08 -0700

    Added bundled redis for cleaner execution

--- start_kafka_redis.sh
+++ start_kafka_redis.sh
@@ -2,7 +2,6 @@
 # Script to build the code and Run.
 
 sh dependencies/kafka_2.11-2.2.1/bin/zookeeper-server-start.sh dependencies/kafka_2.11-2.2.1/config/zookeeper.properties&
-
 sh dependencies/kafka_2.11-2.2.1/bin/kafka-server-start.sh dependencies/kafka_2.11-2.2.1/config/server.properties&
-
-chmod  +x dependencies/redis-stable/src/redis-server && dependencies/redis-stable/src/redis-server&
+tar -xvf dependencies/redis-stable.tar
+chmod  +x redis-stable/src/redis-server && redis-stable/src/redis-server&
